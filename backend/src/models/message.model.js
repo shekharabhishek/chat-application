ca3723d64ae7b8475f559f2ef2c4725ee3765647
shekharabhishek <ydvabhishek33@gmail.com>
2025-04-27 01:14:11 +0530

    changes

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -1,37 +1,37 @@
 // Group Model
 import mongoose from "mongoose";
-import Message from "../models/message.model.js";
-const groupSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    description: {
-      type: String,
-      default: "",
-    },
-    admin: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    members: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    groupImage: {
-      type: String,
-      default: "",
-    },
-  },
-  { timestamps: true }
-);
 
-const Group = mongoose.model("Group", groupSchema);
+// const groupSchema = new mongoose.Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+//     description: {
+//       type: String,
+//       default: "",
+//     },
+//     admin: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+//     members: [
+//       {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "User",
+//       },
+//     ],
+//     groupImage: {
+//       type: String,
+//       default: "",
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// const Group = mongoose.model("Group", groupSchema);
 
 // Message Model
 const messageSchema = new mongoose.Schema(
@@ -58,4 +58,4 @@ const messageSchema = new mongoose.Schema(
 
 const Message = mongoose.model("Message", messageSchema);
 
-export { Group, Message };
+export {  Message };
